691979dca09808f8d48abc22dab4cb7c99087a23
Victor Bello <imuetinyanbello@gmail.com>
2024-05-13 21:47:16 +0100

    Revert "fjkn"
    This reverts commit 5ab8661df318fc3f83ddaece4c4dd90436b2b68c.

--- app/savg/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import LocaleSettings from "@/components/locale";
-import Polo from "@/components/Forma";
-import Homer from "@/components/Homer";
-export default function Page() {
-  return <Homer />;
-}
